1cc7931bf7ab15482609a80a66cb6675f04c43e8
ismat27 <adegbiteismail27@gmail.com>
2022-01-09 22:57:53 +0100

    made some files and added some functionalities

--- myapp/views.py
+++ myapp/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render
+from django.shortcuts import redirect #did this for redirecting to another page
+from django.contrib.auth.models import User, auth
+from django.contrib import messages 
 from django.http import HttpResponse
 from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Superb'
-    feature1.details = 'Our services are superb'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Reliable'
-    feature2.details = 'We never disappoint'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Vast'
-    feature3.details = 'We have varieties of services '
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Affordable'
-    feature4.details = 'Are prices are affordable and higly competitive'
-    features = [feature1, feature2, feature3, feature4]
+    features = Feature.objects.all()
     return render(request, 'index.html', {'features': features}) 
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['con-pass']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Someone has taken this email')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username has been taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password2)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect("register")
+    else:
+        return render(request, 'register.html')
 
 def counter(request):
     comment = request.POST['comment']
